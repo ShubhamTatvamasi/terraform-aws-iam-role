ea48347a1719056821b97ef793ac58500789afd8
Shubham Tatvamasi <shubhamtatvamasi@gmail.com>
2021-12-29 15:07:13 +0530

    modified:   main.tf

--- main.tf
+++ main.tf
@@ -64,9 +64,9 @@ resource "aws_iam_role" "cert_manager_route53_iam_role" {
 }
 
 output "cert_manager_route53_iam_policy" {
-  value = aws_iam_policy.cert_manager_route53_iam_policy.0.arn
+  value = var.setup_cert_manager ? aws_iam_policy.cert_manager_route53_iam_policy.0.arn : null
 }
 
 output "cert_manager_route53_iam_role" {
-  value = aws_iam_role.cert_manager_route53_iam_role.0.arn
+  value = var.setup_cert_manager ? aws_iam_role.cert_manager_route53_iam_role.0.arn : null
 }
